9ea95026749b34181b4dbf006795312099410eae
Tiago <tiagojack84@gmail.com>
2020-03-05 21:44:51 -0300

    add new methods in class account

--- Banco/src/banco/banco.java
+++ Banco/src/banco/banco.java
@@ -12,11 +12,19 @@ public class banco {
 		
 		conta a1 = new conta("0001", a, 154678.69, 20000);
 		
-		System.out.println( "Numero da conta: " + a1.numero +
-							"\nTitular: " + a1.titular.nome +
-							"\nSaldo: " + a1.saldo +
-							"\nLimite: " + a1.limite );
-
+		//System.out.println( "Numero da conta: " + a1.numero +
+		//					"\nTitular: " + a1.titular.nome +
+		//					"\nSaldo: " + a1.saldo +
+		//					"\nLimite: " + a1.limite );
+		
+		
+		@SuppressWarnings("unused")
+		boolean caixa;
+			
+		caixa = a1.saca();
+		
+		caixa = a1.deposita();
+		
 	}
 
 }
--- Banco/src/banco/conta.java
+++ Banco/src/banco/conta.java
@@ -1,9 +1,13 @@
 package banco;
 
+import java.text.DecimalFormat;
+
 /*
  * Author: Tiago Onofre
  * class: conta.
  */
+
+import java.util.Scanner;
 
 public class conta 
 {
@@ -22,4 +26,54 @@ public class conta
 		this.saldo = saldo; 
 		this.limite = limite;
 	}
+	
+	// Sacar dinheiro da conta --> deve verificar se o valor requerido é válido( n <= saldo )
+	boolean saca()
+	{
+		@SuppressWarnings("resource")
+		Scanner terminal = new Scanner(System.in);
+		
+		System.out.println("Informe quanto deseja sacar: ");
+		
+		DecimalFormat df2 = new DecimalFormat("#.##");
+		
+		while ( true ) // loop para impedir fazer com que o usuário consiga sacar mesmo após errar o valor desejado.
+		{
+			double valor = terminal.nextDouble();
+			
+			if ( valor > this.saldo )
+			{
+				System.out.println("Saldo insuficiente! Por favor informe uma quantia válida.");
+				continue;
+			}
+			else
+			{
+				this.saldo = this.saldo - valor;
+				
+				System.out.println("Operação bem sucedida!\nSaldo atual: " + df2.format(this.saldo));
+				
+				return true;
+			}
+		}
+	}
+	
+	// Deve aumentar o saldo --> até o momento sem necessidade verificação.
+	boolean deposita()
+	{
+		@SuppressWarnings("resource")
+		Scanner terminal = new Scanner(System.in);
+		
+		System.out.println("Valor a ser depositado: ");
+		
+		double valor = terminal.nextDouble();
+		
+		this.saldo = this.saldo + valor;
+		
+		DecimalFormat df2 = new DecimalFormat("#.##");
+		
+		System.out.println("Valor depositado com sucesso!\nSaldo atual: " + df2.format(this.saldo) );
+		
+		return true;
+	}
+	
 }
